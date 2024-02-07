fb56362f361aa77e49bfa76c71751921e5597980
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-02-08 01:09:31 +0530

    Update GFG Print Bracket Number.cpp

--- Stacks/GFG Print Bracket Number.cpp
+++ Stacks/GFG Print Bracket Number.cpp
@@ -1,1 +1,21 @@
 
+
+class Solution{
+public:	
+	vector<int> bracketNumbers(string S){
+	  vector<int>ans;
+	  int count = 0;
+	  stack<int>st;
+	  for(int i =0;i<S.size();i++){
+	      if(S[i]=='('){
+	          count++;
+	          ans.push_back(count);
+	          st.push(count);
+	      }else if (S[i]==')'){  // S[i]==')'
+	            ans.push_back(st.top()); 
+	            st.pop();
+	      }
+	  }
+	  return ans;
+	}
+};
