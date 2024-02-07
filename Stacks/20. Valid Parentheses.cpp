d3ccc9826c823d8d9a3f2f09991cbb4f40f34db3
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-02-07 23:07:06 +0530

    Create 20. Valid Parentheses.cpp

---
+++ Stacks/20. Valid Parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(int i =0;i<s.size();i++){
+            if(st.empty() || s[i]=='(' || s[i]=='{' || s[i] =='[') st.push(s[i]);
+            else{
+                if(s[i]==')') {
+                    if(st.top()=='(') st.pop();
+                    else return false;
+                }
+                  if(s[i]=='}') {
+                    if(st.top()=='{') st.pop();
+                    else return false;
+                }
+                  if(s[i]==']') {
+                    if(st.top()=='[') st.pop();
+                    else return false;
+                }
+
+            }
+        }
+        return st.empty();
+    }
+};
+
