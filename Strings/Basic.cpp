63629052d7029c2bf9444ebf79f80fe96dacf6de
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-02-08 23:35:40 +0530

    Update Basic.cpp

--- Strings/Basic.cpp
+++ Strings/Basic.cpp
@@ -15,3 +15,21 @@ int main() {
   ch[2]='\0';
   cout<<ch;
 }
+
+*********************polimdrome**************************************
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+ string s = "naman";
+ int i = 0;
+ int j = s.size()-1;
+ while(i<=j){
+     if(s[i] !=s[j]){
+         cout<<"not a polimdrome"<<endl;
+         break;
+     }
+     i++;
+     j--;
+ }
+ cout<<"a polimdrome"<<endl;
+}
