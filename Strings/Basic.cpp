55fe411708a7c09bf4b06ba01fcfb43aecc9003d
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-02-08 22:54:30 +0530

    Create Basic.cpp

---
+++ Strings/Basic.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+  char ch[10];
+  cin>>ch;
+  cout<<ch;
+}
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+  char ch[10];
+  cin>>ch;
+  ch[2]='\0';
+  cout<<ch;
+}
