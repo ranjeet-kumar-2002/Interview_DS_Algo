68b26d7ea2240cb7a732775b92577a785838f7f7
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-02-09 01:13:15 +0530

    Update GFG Sort a String.cpp

--- Strings/GFG Sort a String.cpp
+++ Strings/GFG Sort a String.cpp
@@ -1,11 +1,11 @@
 
 string sort(string s){
-  vector<int>mp(26,0);
-  for(int i =0;i<s.size();i++){
+  vector<int>mp(26,0); //O(26)
+  for(int i =0;i<s.size();i++){ //O(n)
       mp[s[i]-'a']++;
   }
   string ans = "";
-  for(int i =0;i<mp.size();i++){
+  for(int i =0;i<mp.size();i++){   //O(n)
       char ch = i+'a';
       while(mp[i]--){
           ans+=ch;
