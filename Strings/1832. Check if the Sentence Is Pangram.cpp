11a846dbc7f98431cd1452fa4c1aff5ec3464ad1
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-02-09 01:49:33 +0530

    Update 1832. Check if the Sentence Is Pangram.cpp

--- Strings/1832. Check if the Sentence Is Pangram.cpp
+++ Strings/1832. Check if the Sentence Is Pangram.cpp
@@ -67,7 +67,7 @@ public:
     }
 };
 
-*****************************************************************************************8
+**********************************************************************************************************
 
   
 class Solution {
@@ -81,3 +81,41 @@ public:
       return true;
     }
 };
+
+********************************************************************************************
+
+
+class Solution {
+public:
+    int longestPalindrome(string s) {
+       vector<int>lower(26,0),upper(26,0);
+       for(int i =0;i<s.size();i++){
+           if(s[i]>='a'){  // lower
+            lower[s[i]-'a']++;
+           }else{ // upper
+            upper[s[i]-'A']++;
+           }
+       }
+       int count = 0;
+       int oddcount = 0;
+       for(int i =0;i<26;i++){
+           if(lower[i]%2!=0){
+               oddcount++;
+               count+=lower[i]-1;
+           }else{
+               count +=lower[i];
+           }
+       }
+       for(int i =0;i<26;i++){
+           if(upper[i]%2!=0){
+               oddcount++;
+               count +=upper[i]-1;
+           }else {
+               count +=upper[i];
+           }
+       }
+       if(oddcount>0) count++;
+       return count;
+    }
+};
+
