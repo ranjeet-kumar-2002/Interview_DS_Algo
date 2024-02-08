bcb34c8f3a283442f5f13b58a16216c6b250c29a
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-02-08 17:54:21 +0530

    Update GFG Next Greater Element.cpp

--- Stacks/GFG Next Greater Element.cpp
+++ Stacks/GFG Next Greater Element.cpp
@@ -1,5 +1,12 @@
 ///////////// Next Greater Element/nearest element from the right////////////////////////////////////
+///////////// Next Smallest Element/Nearest element from the right///////////////////////////////////
+///////////// Next Smallest Element/Nearest element from the Left///////////////////////////////////
+///////////// Next Greater Element/Nearest element from the Left///////////////////////////////////
 
+
+
+
+///////////// Next Greater Element/nearest element from the right////////////////////////////////////
 class Solution {
   public:
     vector<long long> nextLargerElement(vector<long long> &arr, int n){
@@ -40,7 +47,7 @@ class Solution {
     vector<long long> nextLargerElement(vector<long long> &arr, int n){
        vector<long long> ans(n,-1);
        stack<int>st;
-       for(int i =0;i<n;i++){  //O(n^2)
+       for(int i =0;i<n;i++){  //O(n)
              while( st.size()>0 && arr[st.top()]<arr[i]){
              ans[st.top()] = arr[i];
              st.pop();
@@ -50,4 +57,64 @@ class Solution {
        return ans;
     }
 };
+
+//////////////////////////////////////// Next Smallest Element/Nearest element from the right///////////////////////////////////
+
+#include<bits/stdc++.h>
+#include<stack>
+vector<int> nextSmallerElement(vector<int> &arr, int n){
+     vector<int> ans(n,-1);
+       stack<int>st;
+       for(int i =0;i<n;i++){  //O(n)
+             while( st.size()>0 && arr[st.top()]>arr[i]){
+             ans[st.top()] = arr[i];
+             st.pop();
+         }
+         st.push(i);
+       }
+       return ans;
+}
+
+
+/////////////////// Smallest number on left//////////////////////////////////////////////
+
+class Solution{
+public:
+    vector<int> leftSmaller(int n, int a[]){
+      vector<int> ans(n,-1);
+      stack<int>st;
+      for(int i =n-1;i>=0;i--){
+          while(st.size()>0 && a[st.top()]>a[i]){
+              ans[st.top()] = a[i];
+              st.pop();
+          }
+          st.push(i);
+      }
+      return ans;
+    }
+};
+
+/////////////////// Greater number on left //////////////////////////////////////////////
+
+#include <vector>
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> leftGreater(int n, int a[]) { // Corrected function name to leftGreater
+        vector<int> ans(n, -1); // Initialize answer vector with -1
+        stack<int> st;
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && a[st.top()] < a[i]) {
+                ans[st.top()] = a[i];
+                st.pop();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};
+
+
 
