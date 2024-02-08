76dc16119663ee6be02a9fc1ecca51fbcf763325
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-02-08 16:19:18 +0530

    Update GFG Next Greater Element.cpp

--- Stacks/GFG Next Greater Element.cpp
+++ Stacks/GFG Next Greater Element.cpp
@@ -32,3 +32,22 @@ class Solution {
        return ans;
     }
 };
+
+
+
+class Solution {
+  public:
+    vector<long long> nextLargerElement(vector<long long> &arr, int n){
+       vector<long long> ans(n,-1);
+       stack<int>st;
+       for(int i =0;i<n;i++){  //O(n^2)
+             while( st.size()>0 && arr[st.top()]<arr[i]){
+             ans[st.top()] = arr[i];
+             st.pop();
+            }
+         st.push(i);
+       }
+       return ans;
+    }
+};
+
