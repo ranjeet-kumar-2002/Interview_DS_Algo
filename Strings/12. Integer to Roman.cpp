6c6d49bbc68808f0785a6c557a71ae8e27efb505
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-02-11 00:24:39 +0530

    Update 12. Integer to Roman.cpp

--- Strings/12. Integer to Roman.cpp
+++ Strings/12. Integer to Roman.cpp
@@ -24,3 +24,63 @@ public:
         return ans+mp[next];
     }
 };
+*********************************************************************************************
+class Solution {
+public:
+    int mp(char ch){
+        if(ch=='I') return 1;
+        else if(ch=='V') return 5;
+        else if(ch=='X') return 10;
+        else if(ch=='L') return 50;
+        else if(ch=='C') return 100;
+        else if(ch=='D') return 500;
+        else return 1000;
+        
+    }
+    int romanToInt(string s) {
+        if(s.size()==0) return 0;
+        if(s.size()==1) return mp(s[0]);
+        int ans = 0;
+         char next;
+        for(int i =0;i<s.size()-1;i++){
+            char prev = s[i];
+            next = s[i+1];
+            int prevVal = mp(prev);
+            int nextVal = mp(next);
+            if(prevVal>=nextVal) ans = ans+prevVal;
+            else ans = ans-prevVal;
+        }
+        return ans+mp(next);
+    }
+};
+
+*****************************************************************************************
+class Solution {
+public:
+    int mp(char ch){
+        if(ch=='I') return 1;
+        else if(ch=='V') return 5;
+        else if(ch=='X') return 10;
+        else if(ch=='L') return 50;
+        else if(ch=='C') return 100;
+        else if(ch=='D') return 500;
+        else return 1000;
+        
+    }
+    int romanToInt(string s) {
+        if(s.size()==0) return 0;
+        if(s.size()==1) return mp(s[0]);
+        int ans = 0;
+        int i = 0;
+        while(i<s.size()-1){
+            char prev = s[i];
+            char next = s[i+1];
+            int prevVal = mp(prev);
+            int nextVal = mp(next);
+            if(prevVal>=nextVal) ans = ans+prevVal;
+            else ans = ans-prevVal;
+            i++;
+        }
+        return ans+mp(s[i]);
+    }
+};
