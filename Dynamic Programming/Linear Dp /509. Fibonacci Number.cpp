7e65742fd431a2f701c9997b87ba2ce8455d4c25
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-02-12 02:06:46 +0530

    Update 509. Fibonacci Number.cpp

--- Dynamic Programming/Linear Dp /509. Fibonacci Number.cpp
+++ Dynamic Programming/Linear Dp /509. Fibonacci Number.cpp
@@ -1,51 +1,54 @@
-// class Solution {
-// public:
-//     int fib(int n) {
-//         if(n==0) return 0;
-//         if(n==1) return 1;
-//         return fib(n-1)+fib(n-2);
-//     }
-// };
 
-// class Solution {
-// public:
-//     int solve(int n ,vector<int>&dp){
-//         if(n==0) return 0;
-//         if(n==1) return 1;
-//         if(dp[n] !=-1) return dp[n];
-//         dp[n] = solve(n-1,dp)+solve(n-2,dp);
-//         return dp[n];
-//     }
-//     int fib(int n) {
-//         if(n==0) return 0;
-//         if(n==1) return 1;
-//         vector<int>dp(n+1,-1);
-//         return solve(n,dp);
-//     }
-// };
+class Solution {
+public:
+    int fib(int n) {
+        if(n==0) return 0;
+        if(n==1) return 1;
+        return fib(n-1)+fib(n-2);
+    }
+};
+**************************************************************************************
+class Solution {
+public:
+    int solve(int n ,vector<int>&dp){
+        if(n==0) return 0;
+        if(n==1) return 1;
+        if(dp[n] !=-1) return dp[n];
+        dp[n] = solve(n-1,dp)+solve(n-2,dp);
+        return dp[n];
+    }
+    int fib(int n) {
+        if(n==0) return 0;
+        if(n==1) return 1;
+        vector<int>dp(n+1,-1);
+        return solve(n,dp);
+    }
+};
 
-
-// class Solution {
-// public:
-//     int solve(int n){
-//          vector<int>dp(n+1,-1);
-//          dp[0] = 0;
-//          dp[1] = 1;
-//         for(int i = 2;i<=n;i++){
-//            dp[i] = dp[i-1]+dp[i-2];
-//         }
+***********************************************************************************
        
-//         return dp[n];
-//     }
-//     int fib(int n) {
-//         if(n==0) return 0;
-//         if(n==1) return 1;
-//         return solve(n);
-//     }
-// };
+class Solution {
+public:
+    int solve(int n){
+         vector<int>dp(n+1,-1);
+         dp[0] = 0;
+         dp[1] = 1;
+        for(int i = 2;i<=n;i++){
+           dp[i] = dp[i-1]+dp[i-2];
+        }
+       
+        return dp[n];
+    }
+    int fib(int n) {
+        if(n==0) return 0;
+        if(n==1) return 1;
+        return solve(n);
+    }
+};
 
 
-
+*****************************************************************************************
+       
 class Solution {
 public:
     int fib(int n) {
