15dff23df0a2494c76d9a2780bbf2634d429fc68
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-02-13 18:40:00 +0530

    Update 2108. Find First Palindromic String in the Array.cpp

--- Standard Template Library (STL)/2108. Find First Palindromic String in the Array.cpp
+++ Standard Template Library (STL)/2108. Find First Palindromic String in the Array.cpp
@@ -1,14 +1,9 @@
 class Solution {
 public:
     bool isPolimdrome(string &s){
-        int i = 0;
-        int j = s.size()-1;
-        while(i<j){
-            if(s[i] !=s[j]) return false;
-            i++;
-            j--;
-        }
-        return true;
+        string temp = s;
+        reverse(temp.begin(),temp.end());
+        return temp == s;
     }
     string firstPalindrome(vector<string>& words) {
         for(int i = 0;i<words.size();i++){
