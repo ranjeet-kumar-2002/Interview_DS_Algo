23d189090af9fdd19dad0b58e9c77cc4b3776bc2
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-02-18 08:24:48 +0530

    Update 252. Meeting Rooms.cpp

--- Sorting/252. Meeting Rooms.cpp
+++ Sorting/252. Meeting Rooms.cpp
@@ -1,1 +1,15 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+bool canattend( vector<vector<int>> &intervals){
+    sort(intervals.begin(),intervals.end());
+    for(int i =1;i<intervals.size();i++){
+        if(intervals[i][0]<intervals[i-1][1]) return false;
+    }
+    return true;
+}
+int main(){
+   // vector<vector<int>> intervals = {{0,30},{5,10},{15,20}};
+    vector<vector<int>> intervals = {{7,10},{2,4}};
+    cout<<canattend(intervals);
+}
