160ae4c34969ba2fc452e653ed7918ae38d8de6b
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-02-20 22:39:02 +0530

    Update Move all negative elements to end.cpp

--- Arrays/Move all negative elements to end.cpp
+++ Arrays/Move all negative elements to end.cpp
@@ -1,3 +1,31 @@
+*****************************************************************************8
+
+    
+class Solution{
+    public:
+    void segregateElements(int arr[],int n){
+     vector<int>temp1;
+     vector<int>temp2;
+     for(int i =0;i<n;i++){
+         if(arr[i]>=0) temp1.push_back(arr[i]);
+     }
+     for(int i =0;i<n;i++){
+         if(arr[i]<0) temp2.push_back(arr[i]);
+     }
+     int j = 0;
+     for(int i =0;i<temp1.size();i++){
+         arr[j] = temp1[i];
+         j++;
+     }
+     for(int i =0;i<temp2.size();i++){
+         arr[j] =temp2[i];
+         j++;
+     }
+     
+    }
+};
+***********************************************************************************88
+
 class Solution{
     public:
     void segregateElements(int arr[],int n){
