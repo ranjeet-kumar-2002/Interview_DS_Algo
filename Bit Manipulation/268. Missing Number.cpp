af42a7378507273c5d45a5a2c812b2471fe560a0
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-02-20 10:05:39 +0530

    Create 268. Missing Number.cpp

---
+++ Bit Manipulation/268. Missing Number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+       int n = nums.size();
+       int sum = 0;
+       int naturesum = n*(n+1)/2;
+       for(int i =0;i<n;i++){
+           sum +=nums[i];
+       }
+       return naturesum-sum;
+    }
+};
+**************************************************************8888
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == i) continue; 
+             else return i;
+        }
+        return n; // If no number is missing, return n
+    }
+};
