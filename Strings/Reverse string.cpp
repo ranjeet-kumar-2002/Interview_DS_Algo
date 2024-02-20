eba976ac1ecac0c386ef04a6b06ed0b726262852
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-02-20 23:08:58 +0530

    Create Reverse string.cpp

---
+++ Strings/Reverse string.cpp
@@ -0,0 +1,13 @@
+class Solution{
+    public:
+    string reverseWord(string str){
+     int i = 0;
+     int j = str.size()-1;
+     while(i<=j){
+         swap(str[i],str[j]);
+         i++;
+         j--;
+     }
+     return str;
+    }
+};
