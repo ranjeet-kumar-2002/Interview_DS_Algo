2c9cdeb966aac9c1ea764e86f3acc0693182b93c
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-02-20 10:36:46 +0530

    Update 268. Missing Number.cpp

--- Bit Manipulation/268. Missing Number.cpp
+++ Bit Manipulation/268. Missing Number.cpp
@@ -23,3 +23,36 @@ public:
         return n; // If no number is missing, return n
     }
 };
+**************************************************************8
+class Solution {
+    public int missingNumber(int[] nums) {
+      int Xor = nums.size();
+      for(int i = 0;i<nums.size();i++){
+          Xor^=i;
+          Xor^=nums[i];
+      }
+      return Xor;
+    }
+}
+
+*************************java**************************************
+    
+class Solution {
+    public int missingNumber(int[] nums) {
+      int Xor = nums.length;
+      for(int i = 0;i<nums.length;i++){
+          Xor^=i;
+          Xor^=nums[i];
+      }
+      return Xor;
+
+    }
+}
+**************************************python*************************************
+    class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        sumnature = n*(n+1)//2  # // is the floor division operator in Python 
+                                # / is the float division operator
+        numsum = sum(nums)
+        return  sumnature-numsum
