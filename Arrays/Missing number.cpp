7cce9f5e0d6f45b73c671031318e4d8db1cf5146
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-02-20 22:07:59 +0530

    Create Missing number.cpp

---
+++ Arrays/Missing number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    int missingNumber(int n, vector<int> &arr) {
+     int naturesum = n*(n+1)/2;
+     int sum = 0;
+     for(int i =0;i<n-1;i++){
+         sum +=arr[i];
+     }
+     return naturesum-sum;
+    }
+};
