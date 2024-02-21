da5dd100365b8351e8fd77a360da8b19e1edaff7
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-02-22 00:25:37 +0530

    Update 3. Longest Substring Without Repeating Characters.cpp

--- Sliding Window/3. Longest Substring Without Repeating Characters.cpp
+++ Sliding Window/3. Longest Substring Without Repeating Characters.cpp
@@ -1,1 +1,21 @@
 
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.size();
+        int i = 0;
+        int j = 0;
+        int len =0;
+        vector<int>count(250,0); //consists of English letters, digits, symbols and spaces.
+        while(j<n){
+            while(count[s[j]]){
+                count[s[i]]=0;
+                i++;
+            }
+            count[s[j]]=1;
+            len =max(len,j-i+1);
+            j++;
+        }
+        return len;
+    }
+};
