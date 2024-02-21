76ea22583d50e1ecaff205b107127b4fd089f615
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-02-22 00:26:26 +0530

    Update 3. Longest Substring Without Repeating Characters.cpp

--- Sliding Window/3. Longest Substring Without Repeating Characters.cpp
+++ Sliding Window/3. Longest Substring Without Repeating Characters.cpp
@@ -6,7 +6,8 @@ public:
         int i = 0;
         int j = 0;
         int len =0;
-        vector<int>count(250,0); //consists of English letters, digits, symbols and spaces.
+        vector<int>count(250,0); // boolen type count
+        //consists of English letters, digits, symbols and spaces.
         while(j<n){
             while(count[s[j]]){
                 count[s[i]]=0;
