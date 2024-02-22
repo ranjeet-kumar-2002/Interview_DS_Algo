7dd4eded08a04871ab8fab330a9bea40a45b9981
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-02-22 17:56:51 +0530

    Update bfs.cpp

--- Graphs/Traversal/bfs.cpp
+++ Graphs/Traversal/bfs.cpp
@@ -1,1 +1,24 @@
+BFS of graph
 
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+       vector<int>ans;
+       vector<bool>visited(V,false);
+       queue<int>q;
+       q.push(0);
+       visited[0] = true;
+       while(!q.empty()){
+           int node = q.front();q.pop();
+           ans.push_back(node);
+           for(auto&nbr:adj[node]){
+               if(!visited[nbr]){
+                   q.push(nbr);
+                   visited[nbr] = true;
+               }
+           }
+       }
+       return ans;
+    }
+};
