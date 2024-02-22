86b6511e34985ba9eb90f15b48c26e2cc21a7e82
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-02-22 21:45:29 +0530

    Update Introduction.cpp

--- Binary Tree/Introduction.cpp
+++ Binary Tree/Introduction.cpp
@@ -1,1 +1,37 @@
-
+*****************creating a binary tree using level order traversal********************************
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+   public:
+  int data;
+  Node*left;
+  Node*right;
+  Node(int value){
+      data = value;
+      left=right=NULL;
+  }
+};
+int main(){
+    cout<<"Enter the root element"<<endl;
+    int x,first,second;
+    cin>>x;
+    Node* root = new Node(x);
+    queue<Node*>q;
+    q.push(root);
+    while(!q.empty()){
+        Node*temp = q.front();q.pop();
+        cout<<"enter the left element"<<temp->data<<endl;
+        cin>>first;
+        if(first!=-1){
+            temp->left = new Node( first);
+            q.push(temp->left);
+        }
+        cout<<"enter the second"<<temp->data<<endl;
+        cin>>second;
+        if(second !=-1){
+            temp->right = new Node(second);
+            q.push(temp->right);
+        }
+    }
+    
+}
