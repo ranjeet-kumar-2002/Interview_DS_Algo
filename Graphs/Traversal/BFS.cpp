358e301020de89b68d2584b2f3fc5088b2f755d9
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-02-22 18:12:32 +0530

    Update BFS.cpp

--- Graphs/Traversal/BFS.cpp
+++ Graphs/Traversal/BFS.cpp
@@ -22,3 +22,31 @@ class Solution {
        return ans;
     }
 };
+
+******************************************************************************************
+
+class Solution {
+  public:
+    void bfs(vector<int>&ans,vector<bool>&visited,queue<int>q,vector<int>adj[]){
+        while(!q.empty()){
+            int node = q.front();q.pop();
+            ans.push_back(node);
+            for(auto &nbr:adj[node]){
+                if(!visited[nbr]){
+                    q.push(nbr);
+                    visited[nbr]=true;
+                }
+            }
+        }
+    }
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+      vector<int>ans;
+      vector<bool>visited(V,false);
+      queue<int>q;
+      q.push(0);
+      visited[0] =true;
+      bfs(ans,visited,q,adj);
+      return ans;
+    }
+};
+
