c710295d05f15e5610215a1fc04b62dd28ddbf11
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-02-24 00:42:44 +0530

    Update Count Leaves in Binary Tree.cpp

--- Binary Tree/Count Leaves in Binary Tree.cpp
+++ Binary Tree/Count Leaves in Binary Tree.cpp
@@ -14,7 +14,7 @@ int f(Node* root){
 int countLeaves(Node* root){
   return f(root);
 }
-
+***************************************************************
 
 int f(Node* root){
     if(root->left==NULL && root->right==NULL) return 1;
@@ -30,3 +30,40 @@ int f(Node* root){
 int countLeaves(Node* root){
    return f(root);
 }
+********************************************************************
+
+
+void f(Node* root,int &count){
+  if(!root) return;
+  if(!root->left && !root->right) count++;
+  if(root->left) f(root->left,count);
+  if(root->right) f(root->right,count);
+    
+}
+int countLeaves(Node* root){
+    int count = 0;
+    f(root,count);
+   return count;
+}
+
+
+
+******************************************************************************
+
+int f(Node* root){
+    if(!root) return 0;
+    if(root->left==NULL && root->right==NULL) return 1;
+    int leftcount = 0;
+    int rightcount= 0;
+    if(root->left) {
+      leftcount +=f(root->left);
+    }
+    if(root->right){
+        rightcount +=f(root->right);
+    }
+    return leftcount+rightcount;
+}
+int countLeaves(Node* root){
+  return f(root);
+}
+
