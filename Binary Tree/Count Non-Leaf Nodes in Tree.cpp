334850547041d69836e3dfc3177f4621b45bacfc
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-02-24 01:18:47 +0530

    Update Count Non-Leaf Nodes in Tree.cpp

--- Binary Tree/Count Non-Leaf Nodes in Tree.cpp
+++ Binary Tree/Count Non-Leaf Nodes in Tree.cpp
@@ -1,17 +1,28 @@
+
 class Solution {
   public:
     int f(Node* root){
-        if(root->left && root->right) return 1;
         int count = 0;
+        if(root->left || root->right) count++;
         if(root->left){
             count +=f(root->left);
         }
         if(root->right){
-            count+=f(root->right);
+            count +=f(root->right);
         }
         return count;
     }
     int countNonLeafNodes(Node* root) {
       return f(root);
+    }
+};
+
+***********************************************************************
+class Solution {
+  public:
+    int countNonLeafNodes(Node* root) {
+       if(!root) return 0;
+       if(!root->left && !root->right) return 0;
+       return 1+countNonLeafNodes(root->left)+countNonLeafNodes(root->right);
     }
 };
