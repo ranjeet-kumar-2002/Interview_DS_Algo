081a346392cacad4aef2f37b6e4226006dbb26b4
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-02-23 16:03:43 +0530

    Create Level order traversal.cpp

---
+++ Binary Tree/Level order traversal.cpp
@@ -0,0 +1,22 @@
+
+
+class Solution
+{
+    public:
+    vector<int> levelOrder(Node* root){
+      queue<Node*>q;
+      q.push(root);
+      vector<int>ans;
+      while(!q.empty()){
+           int size = q.size();
+           for(int i = 0;i<size;i++){
+               Node* temp = q.front();q.pop();
+               ans.push_back(temp->data);
+               if(temp->left) q.push(temp->left);
+               if(temp->right) q.push(temp->right);
+           }
+           
+      }
+      return ans;
+    }
+};
