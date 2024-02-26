456dcb028333365bd7debfaa18a3d503dc03bf32
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-02-26 14:47:39 +0530

    Update 100. Same Tree.cpp

--- Binary Tree/100. Same Tree.cpp
+++ Binary Tree/100. Same Tree.cpp
@@ -1,4 +1,4 @@
-/**
+
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
@@ -8,7 +8,7 @@
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
- */
+ 
        p     q
 step1: NULL  NULL
 step2: NULL  Node
@@ -38,5 +38,24 @@ public:
     }
     bool isSameTree(TreeNode* p, TreeNode* q) {
          return f(p,q);
+    }
+};
+*****************************************************************************
+
+class Solution
+{
+    public:
+    bool f(Node* r1,Node* r2){
+        if(r1==NULL && r2==NULL) return true;
+        if(r1==NULL && r2!= NULL) return false;
+        if(r1!=NULL && r2==NULL) return false;
+        if(r1->data !=r2->data) return false;
+        bool leftpart = f(r1->left,r2->left);
+        bool rightpart = f(r1->right,r2->right);
+        return leftpart && rightpart;
+    }
+    bool isIdentical(Node *r1, Node *r2)
+    {
+        return f(r1,r2);
     }
 };
