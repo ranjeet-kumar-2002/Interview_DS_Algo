8cd63c41f3390f41fbde7817a10e04a2af8867e3
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-02-27 17:13:41 +0530

    Update Problem List.cpp

--- Maths for DSA/Problem List.cpp
+++ Maths for DSA/Problem List.cpp
@@ -1,7 +1,7 @@
-3025. Find the Number of Ways to Place People I ,II
+****3025. Find the Number of Ways to Place People I ,II
 1492. The kth Factor of n.cpp
 Find all divisors of a natural number.cpp
 Find the total no of digit in the given number.cpp
 power of two numbers.cpp
 3024. Type of Triangle
-279. Perfect Squares | 5 Ways | DP | Math | BFS | Lagrange & Legendre Theorem | Top-Down | Bottom Up
+****279. Perfect Squares | 5 Ways | DP | Math | BFS | Lagrange & Legendre Theorem | Top-Down | Bottom Up
