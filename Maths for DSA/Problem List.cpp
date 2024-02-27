f618ab0265af1a9e6f160d507a312d951f970920
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-02-27 16:11:19 +0530

    Update Problem List.cpp

--- Maths for DSA/Problem List.cpp
+++ Maths for DSA/Problem List.cpp
@@ -1,1 +1,5 @@
 3025. Find the Number of Ways to Place People I ,II
+1492. The kth Factor of n.cpp
+Find all divisors of a natural number.cpp
+Find the total no of digit in the given number.cpp
+power of two numbers.cpp
