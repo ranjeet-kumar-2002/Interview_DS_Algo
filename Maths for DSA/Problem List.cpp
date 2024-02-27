e23ace848f6003c5521032644867e4476b884799
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-02-27 17:13:16 +0530

    Update Problem List.cpp

--- Maths for DSA/Problem List.cpp
+++ Maths for DSA/Problem List.cpp
@@ -4,3 +4,4 @@ Find all divisors of a natural number.cpp
 Find the total no of digit in the given number.cpp
 power of two numbers.cpp
 3024. Type of Triangle
+279. Perfect Squares | 5 Ways | DP | Math | BFS | Lagrange & Legendre Theorem | Top-Down | Bottom Up
