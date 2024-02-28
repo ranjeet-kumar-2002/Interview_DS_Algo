52ef3d6b7d7b1c65ba5918d71c1e6cba618bbc54
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-02-28 22:11:54 +0530

    Update Problem List.cpp

--- Maths for DSA/Problem List.cpp
+++ Maths for DSA/Problem List.cpp
@@ -1,7 +1,4 @@
 ****3025. Find the Number of Ways to Place People I ,II
-1492. The kth Factor of n.cpp
-Find all divisors of a natural number.cpp
-Find the total no of digit in the given number.cpp
-power of two numbers.cpp
-3024. Type of Triangle
+****3044. Most Frequent Prime
+****2601. Prime Subtraction Operation
 ****279. Perfect Squares | 5 Ways | DP | Math | BFS | Lagrange & Legendre Theorem | Top-Down | Bottom Up
