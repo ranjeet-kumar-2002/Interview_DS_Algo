d90106f82fc245c72740e9c33bb47456c9f71368
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-02-29 17:03:31 +0530

    Create isEvenOddTree.cpp

---
+++ Binary Tree/isEvenOddTree.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isEvenOddTree(TreeNode* root) {
+        queue<TreeNode*> q;
+         bool is_even = true;
+        q.push(root);
+        while (!q.empty()) {
+            int n = q.size();
+            int prev;
+            if(is_even) prev = INT_MIN;
+            else prev = INT_MAX;
+            for (int i = 0; i < n; i++) {
+               TreeNode* temp = q.front();q.pop();
+               if(is_even && (temp->val%2==0 || temp->val<=prev)) return false;//Evenindx 
+               if(!is_even && (temp->val%2!=0 || temp->val>=prev)) return false; //oddindex
+                prev = temp->val;
+                if(temp->left) q.push(temp->left); 
+                if(temp->right) q.push(temp->right); 
+            }
+           is_even = !is_even;
+        }
+        return true;
+    }
+};
