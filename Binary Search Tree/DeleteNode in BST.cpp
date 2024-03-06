84e6de10351e959752459c2d33152c1e009dad16
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-03-06 19:55:32 +0530

    Create DeleteNode in BST.cpp

---
+++ Binary Search Tree/DeleteNode in BST.cpp
@@ -0,0 +1,46 @@
+Node *deleteNode(Node *root, int X) {
+   if(!root) return NULL;
+   else if(root->data>X){
+       root->left = deleteNode(root->left, X);
+       return root;
+   }else if(root->data<X){
+       root->right =  deleteNode(root->right, X);
+       return root;
+   }else{
+       // if leaf node
+       if(root->right==NULL && root->left==NULL){
+           delete root;
+           return NULL;
+       }   // if one child
+       else if(root->left==NULL){
+           Node* temp = root->right;
+           delete root;
+           return temp;
+       } else if(root->right==NULL){
+           Node* temp = root->left;
+           delete root;
+           return temp;
+       }
+       else{  // if two child
+            // finf the greatest elemtn from left
+            Node* child = root->left;
+            Node* parent = root;
+            // rightmost node tak pahuchna hai
+            while(child->right){
+                parent = child;
+                child= child->right;
+            }
+            if(parent !=root){
+                parent->right = child->left;
+                child->left = root->left;
+                child->right = root->right;
+                return child;
+            }
+            else{
+                child->right = root->right;
+                delete root;
+                return child;
+            }
+       }
+   }
+}
