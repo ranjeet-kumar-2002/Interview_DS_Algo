548a729d3e639f3e7db59706ceb6d3be64219a96
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-03-07 21:51:07 +0530

    Update Stack Using Arrays and LinkedList.cpp

--- Stacks/Stack Using Arrays and LinkedList.cpp
+++ Stacks/Stack Using Arrays and LinkedList.cpp
@@ -1,1 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Stack{
+public:
+    int size;
+    int top;
+    int *arr;
+    
+    Stack(int s){
+        size = s;
+        top = -1;
+        arr = new int[s];
+    }
+    
+    void push(int value){
+        if(top == size - 1){
+            cout << "stack overflow" << endl;
+        } else {
+            top++;
+            arr[top] = value;
+        }
+    }
+    
+    void pop(){
+        if(top == -1){
+            cout << "stack underflow" << endl;
+            return;
+        } else {
+            top--;
+        }
+    }
+    
+    int peek(){
+        if(top == -1){
+            cout << "stack is empty" << endl;
+            return -1;
+        } else {
+            return arr[top];
+        }
+    }
+    
+    bool isEmpty(){
+        return top == -1;
+    }
+    
+    int getSize(){
+        return top + 1;
+    }
+};
+
+int main(){
+    Stack s(3);
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    cout << s.peek() << endl;
+}
+********************************************************************
 
