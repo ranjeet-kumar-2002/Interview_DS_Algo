1801e91bbcb81aa3facf89ba3b80f186ac10b5a5
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-03-07 10:33:30 +0530

    Update 94. Binary Tree Inorder Traversal(morriss).cpp

--- Binary Tree/94. Binary Tree Inorder Traversal(morriss).cpp
+++ Binary Tree/94. Binary Tree Inorder Traversal(morriss).cpp
@@ -1,3 +1,43 @@
+
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        while(root){
+            if(root->left==NULL){  // left not exits
+                ans.push_back(root->val);
+                root= root->right;
+            }else{ //left  exits
+                TreeNode* curr = root->left;
+                while(curr->right && curr->right !=root){
+                    curr = curr->right;
+                }
+                if(curr->right==NULL) { //not travese
+                    curr->right = root;
+                    root= root->left;
+                }else{  // already traverse
+                     curr->right = NULL;
+                     ans.push_back(root->val);
+                     root = root->right;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+*************************************************************************
+
+
+
+
+
+
+
+
+
+
 //94. Binary Tree Inorder Traversal
 class Solution {
 public:
