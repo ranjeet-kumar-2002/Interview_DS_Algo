66559189c156273439092924c7bcd35990e9824e
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-03-08 00:57:04 +0530

    Update Stack Using Arrays and LinkedList.cpp

--- Stacks/Stack Using Arrays and LinkedList.cpp
+++ Stacks/Stack Using Arrays and LinkedList.cpp
@@ -58,3 +58,71 @@ int main(){
 }
 ********************************************************************
 
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int value){
+        data = value;
+        next = NULL;
+    }
+};
+class Stack{
+    Node* top;
+    int size;
+    public:
+    Stack(){
+        top= NULL;
+        size = 0;
+    }
+    void push(int value){
+        Node* temp = new Node(value);
+        if(temp==NULL) {
+            cout<<"stack is overflow"<<endl;
+            return;
+        }
+        else{   
+                temp->next = top;
+                cout<<"pushed"<<value<<" "<<endl;
+                top = temp;
+                size++;
+        }
+    }
+    
+    void pop(){
+        if(top==NULL){
+            cout<<"stack underflow"<<endl;
+        }else{
+            
+            Node* temp = top;
+            top = top->next;
+            delete temp;
+            size--;
+        }
+    }
+    int peek(){
+        if(top==NULL) {
+            cout<<"stack is empty"<<endl;
+            return -1;
+        } else return top->data;
+    }
+    bool isempty(){
+        return top==NULL;
+    }
+    int issize(){
+        return size;
+    }
+};
+int main(){
+    
+    Stack s;
+    s.push(6);
+    s.push(16);
+    s.push(62);
+    s.push(86);
+}
+
+
