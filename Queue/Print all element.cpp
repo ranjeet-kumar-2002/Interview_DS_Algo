dc39ec566fc6d27c57348bac6cb1d22ab24984ac
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-03-07 18:33:47 +0530

    Update Print all element.cpp

--- Queue/Print all element.cpp
+++ Queue/Print all element.cpp
@@ -1,1 +1,17 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    queue<int>q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+    int n = q.size();
+    while(n--){
+        cout<<q.front()<<" "; q.pop();
+        q.push(q.front());
+    }
+    cout<<endl;
+    cout<<q.size();
+}
