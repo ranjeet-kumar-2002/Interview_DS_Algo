8972e744040204553681445791993bb91c7ed433
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-03-07 18:47:54 +0530

    Update Reverse first K element of queue.cpp

--- Queue/Reverse first K element of queue.cpp
+++ Queue/Reverse first K element of queue.cpp
@@ -1,3 +1,25 @@
+class Solution{
+    public:
+    queue<int> modifyQueue(queue<int> q, int k) {
+       int n = q.size()-k;
+       stack<int>st;
+       while(k--){
+           st.push(q.front());q.pop();
+       }
+       while(!st.empty()){
+           q.push(st.top());st.pop();
+       }
+       
+       while(n--){
+           q.push(q.front());q.pop();
+       }
+       return q;
+    }
+};
+
+********************************************************888
+
+
 class Solution
 {
     public:
