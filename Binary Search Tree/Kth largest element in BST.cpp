3e3064ad259e8e991b805c9847f8fcf962773a1d
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-03-08 20:50:33 +0530

    Create Kth largest element in BST.cpp

---
+++ Binary Search Tree/Kth largest element in BST.cpp
@@ -0,0 +1,36 @@
+//Kth largest element in BST
+
+// class Solution{
+//     public:
+//     void f(Node* root,vector<int>&ans){
+//         if(!root) return;
+//         f(root->left,ans);
+//         ans.push_back(root->data);
+//         f(root->right,ans);
+//     }
+//     int kthLargest(Node *root, int K){
+//       vector<int>ans;
+//       f(root,ans);
+//       int n = ans.size();
+//       return ans[n-K];
+//     }
+// };
+
+class Solution{
+    public:
+    void f(Node* root,int &ans,int &k){
+        if(!root) return;
+        f(root->right,ans,k);
+        k--;
+        if(k==0){
+            ans = root->data;
+            return;
+        }
+        f(root->left,ans,k);
+    }
+    int kthLargest(Node *root, int K){
+      int ans = 0;
+      f(root,ans,K);
+      return ans;
+    }
+};
