cf6dbada286bda78250c8e3300e9ea800d840cdc
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-03-10 01:45:14 +0530

    Update kthsmallest(.cpp

--- Binary Search Tree/kthsmallest(.cpp
+++ Binary Search Tree/kthsmallest(.cpp
@@ -1,13 +1,26 @@
 
-void f(TreeNode* A,int &ans,int &B){
-    if(!A) return;
-    f(A->left,ans,B);
-    B--;
-    if(B==0) {
-        ans = A->val;
-        return;
-    }
-     f(A->right,ans,B);
+void f(TreeNode* root, vector<int>&ans){
+  if(!root) return;
+  f(root->left,ans);
+  ans.push_back(root->val);    
+  f(root->right,ans);    
+}
+int Solution::kthsmallest(TreeNode* A, int B) {
+    vector<int>ans;
+    f(A,ans);
+    return ans[B-1];
+}
+
+*********************************************8
+void f(TreeNode* root, int &ans,int &k){
+  if(!root) return;
+  f(root->left,ans,k);
+  k--;
+  if(k==0){
+      ans = root->val;
+      return;
+  }  
+  f(root->right,ans,k);    
 }
 int Solution::kthsmallest(TreeNode* A, int B) {
     int ans = 0;
