a586350a0bf79ce7956549d65d029a8a9b26976e
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-03-10 01:44:11 +0530

    Create Inorder Traversal and BST.cpp

---
+++ Binary Search Tree/Inorder Traversal and BST.cpp
@@ -0,0 +1,14 @@
+//Inorder Traversal and BST
+
+class Solution{
+    public:
+    int isRepresentingBST(int arr[], int N)  {
+      for(int i =0;i<N-1;i++){
+          if(arr[i]>=arr[i+1]) {
+              return false;
+              break;
+          }
+      }
+      return true;
+    }
+};
