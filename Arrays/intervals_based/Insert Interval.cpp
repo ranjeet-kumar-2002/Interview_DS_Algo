4d7563a31367a635277fc1d2430eac8d210a9916
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-03-18 11:03:52 +0530

    Create Insert Interval.cpp

---
+++ Arrays/intervals_based/Insert Interval.cpp
@@ -0,0 +1,29 @@
+//57. Insert Interval
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals,
+                               vector<int>& newInterval) {
+        int i = 0;
+        vector<vector<int>> ans;
+        int n = intervals.size();
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            ans.push_back(intervals[i]);
+            i++;
+        }
+        while (i < n && ((intervals[i][0] <= newInterval[0] &&
+                          intervals[i][1] >= newInterval[0]) ||
+                         (intervals[i][0] >= newInterval[0] &&
+                          intervals[i][0] <= newInterval[1]))) {
+            newInterval[0] = min(intervals[i][0], newInterval[0]);
+            newInterval[1] = max(intervals[i][1], newInterval[1]);
+            i++;
+        }
+
+        ans.push_back(newInterval);
+        while (i < n) {
+            ans.push_back(intervals[i]);
+            i++;
+        }
+        return ans;
+    }
+};
