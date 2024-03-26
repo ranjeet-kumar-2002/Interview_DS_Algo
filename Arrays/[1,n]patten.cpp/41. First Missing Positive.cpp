7db280e1178ab496c37cd1b7be79af6b0d8e66ef
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-03-27 01:07:25 +0530

    Update 41. First Missing Positive.cpp

--- Arrays/[1,n]patten.cpp/41. First Missing Positive.cpp
+++ Arrays/[1,n]patten.cpp/41. First Missing Positive.cpp
@@ -1,19 +1,19 @@
 //41. First Missing Positive
-// class Solution {
-// public:
-//     int firstMissingPositive(vector<int>& nums) {
-//     int n = nums.size();
-//        unordered_set<int>st;
-//        for(int i = 0;i<n;i++){
-//          if(nums[i]>0) st.insert(nums[i]);
-//        }
-//        for(int i =1;i<=100001;i++){
-//          if(st.find(i) == st.end()) return i;
-//        }
-//        return 0;
-//     }
-// };
-
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+    int n = nums.size();
+       unordered_set<int>st;
+       for(int i = 0;i<n;i++){
+         if(nums[i]>0) st.insert(nums[i]);
+       }
+       for(int i =1;i<=100001;i++){
+         if(st.find(i) == st.end()) return i;
+       }
+       return 0;
+    }
+};
+****************************************************************
 
 // This problem also related to the [1,n] patterns
 class Solution {
