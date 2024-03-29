5fb1d0ee9c7c49a7b514f8ab35b4c475557e6158
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-03-29 14:54:20 +0530

    Update Selection Sort.cpp

--- Algorithms/Selection Sort.cpp
+++ Algorithms/Selection Sort.cpp
@@ -1,1 +1,97 @@
 
+#include<iostream>
+#include<vector>
+#include<cmath>
+#include<climits>
+#include<algorithm>
+using namespace std;
+void selectionSort(int arr[],int n){
+     for(int i = 0;i<n-1;i++){
+          int smallestIndx = i;
+          int smallest = arr[i];
+          for(int j = i+1;j<n;j++){
+              if(smallest>arr[j]){
+                     smallestIndx = j;
+                     smallest = arr[j];
+              }
+          }
+          swap(arr[i],arr[smallestIndx]);
+     }
+}
+int main(){
+     int n; cin>>n;
+     int arr[n];
+     for(int i =0;i<n;i++){
+         cin>>arr[i];
+     }
+    selectionSort(arr,n);
+     for(int i =0;i<n;i++){
+         cout<<arr[i]<<" ";
+     }
+}
+************************************************************************
+#include<iostream>
+#include<vector>
+#include<cmath>
+#include<climits>
+#include<algorithm>
+using namespace std;
+void selectionSort(int arr[],int n){
+     for(int i = 0;i<n-1;i++){
+          int smallestIndx = i;
+          int smallest = arr[i];
+          bool flag = false; 
+          for(int j = i+1;j<n;j++){
+              if(smallest>arr[j]){
+                     smallestIndx = j;
+                     smallest = arr[j];
+                     flag = true;
+              }
+          }
+          if(flag) swap(arr[i],arr[smallestIndx]);
+     }
+}
+int main(){
+     int n; cin>>n;
+     int arr[n];
+     for(int i =0;i<n;i++){
+         cin>>arr[i];
+     }
+    selectionSort(arr,n);
+     for(int i =0;i<n;i++){
+         cout<<arr[i]<<" ";
+     }
+}
+************************************************************************************
+#include<iostream>
+#include<vector>
+#include<cmath>
+#include<climits>
+#include<algorithm>
+using namespace std;
+void selectionSort(int arr[],int n){
+     for(int i = 0;i<n-1;i++){
+          int smallestIndx = i;
+          for(int j = i+1;j<n;j++){
+              if(arr[smallestIndx]>arr[j]){
+                     smallestIndx = j;
+              }
+          }
+          swap(arr[i],arr[smallestIndx]);
+     }
+}
+int main(){
+     int n; cin>>n;
+     int arr[n];
+     for(int i =0;i<n;i++){
+         cin>>arr[i];
+     }
+    selectionSort(arr,n);
+     for(int i =0;i<n;i++){
+         cout<<arr[i]<<" ";
+     }
+}
+
+
+// 5
+// 9 7 3 1 6
