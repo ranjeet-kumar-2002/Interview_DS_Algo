2935dce825f2914d037b39ee572beb72001d4c52
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-03-29 10:12:02 +0530

    Update 229. Majority Element II.cpp

--- Arrays/Boyer-Moore_voting algo.cpp/229. Majority Element II.cpp
+++ Arrays/Boyer-Moore_voting algo.cpp/229. Majority Element II.cpp
@@ -1,3 +1,55 @@
+// Input: nums = [3,2,3]
+// Output: [3]
+
+#include<iostream>
+#include<vector>
+#include<cmath>
+#include<climits>
+using namespace std;
+vector<int> majority(vector<int>&nums){
+     int count1 = 0,count2 = 0,maj2 = 0,maj1= 0;
+      int n = nums.size();
+     for(int i = 0;i<n;i++){
+          if(maj1==nums[i]) count1++;
+          else if(maj2==nums[i]) count2++;
+          else if(count1==0){
+               maj1 = nums[i];
+               count1 = 1;
+          }else if(count2==0){
+               maj2 = nums[i];
+               count2 = 1;
+          }else {
+               count1--;
+               count2--;
+          }
+     }
+     // verification
+     count1=0; count2 = 0;
+     for(int i = 0;i<n;i++){
+          if(nums[i]==maj1) count1++;
+          if(nums[i]==maj2) count2++;
+     }
+     vector<int>ans;
+    if(n/3<count1) ans.push_back(maj1);
+    if(n/3<count2) ans.push_back(maj2);
+    return ans;
+}
+int main(){
+     int n;cin>>n;
+     vector<int>nums(n);
+     for(int i = 0;i<n;i++){
+          cin>>nums[i];
+     }
+     vector<int> temp = majority(nums);
+     for(int i= 0;i<temp.size();i++){
+          cout<<temp[i]<<endl;
+     }
+     return 0;
+}
+
+*********************************************************************************************
+
+
 229. Majority Element II
 class Solution {
 public:
@@ -13,7 +65,7 @@ public:
     }
 };
 
-
+*******************************************************************************************
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
