001bf6a29f3efa888d9878bff9e3ea302b7ae18d
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-03-30 18:36:42 +0530

    Update Sqrt(x).cpp

--- Arrays/binary search/Sqrt(x).cpp
+++ Arrays/binary search/Sqrt(x).cpp
@@ -1,23 +1,55 @@
-// class Solution {
-// public:
-//     int mySqrt(int x) {
-//         int l = 1;int r = x;
-//         int target = x;
-//         long long ans = 0;
-//         while(l<=r){
-//              long long mid = l+(r-l)/2;
-//              if(mid*mid==target){
-//                  ans = mid;break;
-//              }
-//             else if(mid*mid<target){
-//                  ans = mid;
-//                  l = mid+1;
-//              }else r = mid-1;
-//         }
-//         return ans;
-//     }
-// };
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<cmath>
+#include<climits>
+#include<algorithm>
+using namespace std;
+int sqrt(int n){
+    if(n<2) return n;
+     int l = 1;
+     int r = n;
+     int target = n;
+     int ans = 0;
+     while(l<=r){
+          int mid = l+(r-l)/2;
+          if(mid*mid==target) {
+               ans = mid; break;
+          }else if(mid*mid<target){
+               ans = mid;
+               l = mid+1;
+          }else r = mid-1;
+     }
+     return ans;
+}
+int main(){
+     int n;cin>>n;
+     cout<<sqrt(n);
+     return 0;
+}
+******************************************************************************
 
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        int l = 1;int r = x;
+        int target = x;
+        long long ans = 0;
+        while(l<=r){
+             long long mid = l+(r-l)/2;
+             if(mid*mid==target){
+                 ans = mid;break;
+             }
+            else if(mid*mid<target){
+                 ans = mid;
+                 l = mid+1;
+             }else r = mid-1;
+        }
+        return ans;
+    }
+};
+*************************************************************************************
 class Solution {
 public:
     int mySqrt(int x) {
