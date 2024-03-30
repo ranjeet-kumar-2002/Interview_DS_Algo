04a6d4f25cd790a4d064e9eeff0ccb1438453bc0
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-03-30 19:03:02 +0530

    Update Cube root of a number.cpp

--- Arrays/binary search/Cube root of a number.cpp
+++ Arrays/binary search/Cube root of a number.cpp
@@ -1,3 +1,34 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<cmath>
+#include<climits>
+#include<algorithm>
+
+using namespace std;
+int cuberoot(int n){
+     int l  =1;
+     int r = n;
+     int ans = 0;
+     while(l<=r){
+          int mid = l+(r-l)/2;
+          if(mid*mid*mid==n) return mid;
+          else if(mid<=n/mid/mid) {
+              ans = mid;
+              l = mid+1;
+          }else r = mid-1;
+     }
+     return ans;
+}
+int main(){
+    int n;cin>>n;
+    cout<<cuberoot(n);
+    return 0;
+}
+
+*************************************************************************************
+
+
 //Cube root of a number
 class Solution {
 public:
