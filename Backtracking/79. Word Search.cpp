a9bac82cd883d832bbe5b6c42974dea4a29e0bcf
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-04-03 21:35:20 +0530

    Update 79. Word Search.cpp

--- Backtracking/79. Word Search.cpp
+++ Backtracking/79. Word Search.cpp
@@ -1,3 +1,38 @@
+class Solution {
+public:
+    int m;
+    int n;
+    vector<vector<int>> dir = {{0,1},{1,0},{-1,0},{0,-1}};
+    bool find(int i,int j ,int indx,string &word , vector<vector<char>>& board){
+         if(indx == word.size()) return true;
+         if(i<0 || i>=m || j<0 || j>=n || board[i][j] =='$') return false;
+         if(board[i][j] !=word[indx]) return false;
+         char temp = board[i][j];
+         board[i][j] = '$';
+         for(int d = 0;d<4;d++){
+             int newi = i+dir[d][0];
+             int newj = j+dir[d][1];
+             if(find(newi,newj,indx+1,word,board)) return true;
+         }
+        board[i][j] = temp;
+        return false;
+
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        m = board.size();
+        n = board[0].size();
+        for(int i =0;i<m;i++){
+             for(int j = 0;j<n;j++){
+                 if(board[i][j]==word[0] && find(i,j,0,word,board)) return true;
+             }
+        }
+        return false;
+    }
+};
+
+***********************************************************************************************
+
+
 #include <iostream>
 #include <vector>
 #include <string>
