932593fb22f9dded294d89bad64ac771cd525247
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-04-07 23:54:50 +0530

    Update Dynamic Memory Allocation of 2D and 3D Arrays.cpp

--- Algorithms/Pointers/Dynamic Memory Allocation of 2D and 3D Arrays.cpp
+++ Algorithms/Pointers/Dynamic Memory Allocation of 2D and 3D Arrays.cpp
@@ -29,3 +29,35 @@ int main(){
     
     return 0;
 }
+***************************************************************************************
+#include<iostream>
+using namespace std;
+int main(){
+    int L,B,H;
+    cin>>L>>B>>H;
+    int ***ptr = new int **[L];
+    for(int i =0;i<L;i++){
+         ptr[i] = new int *[B];
+         for(int j =0;j<B;j++){
+              ptr[i][j]= new int[H];
+         }
+    }
+    
+    for(int i =0;i<L;i++){
+        for(int j =0;j<B;j++){
+            for(int k = 0;k<H;k++){
+                ptr[i][j][k] = i+j+k;
+            }
+        }
+    }
+     for(int i =0;i<L;i++){
+        for(int j =0;j<B;j++){
+            for(int k = 0;k<H;k++){
+                cout<<ptr[i][j][k]<<" ";
+            }
+        }
+    }
+    
+}
+
+
