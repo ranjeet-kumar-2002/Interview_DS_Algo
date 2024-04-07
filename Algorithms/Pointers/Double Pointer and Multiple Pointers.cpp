deb71dc7468df77b6a19e69f898bac46c1e2f70c
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-04-07 12:18:21 +0530

    Update Double Pointer and Multiple Pointers.cpp

--- Algorithms/Pointers/Double Pointer and Multiple Pointers.cpp
+++ Algorithms/Pointers/Double Pointer and Multiple Pointers.cpp
@@ -13,3 +13,20 @@ int main(){
      cout<<&p3<<endl; //0x7ffe110245c0
      return 0;
 }
+********************************************************
+#include<iostream>
+using namespace std;
+int main(){
+     int n = 10;
+     int *p1 = &n;// single pointer 
+     int **p2 = &p1; // double pointer 
+     int ***p3 = &p2; // triple pointer
+     
+     *p1 = *p1+5;
+     cout<<n<<endl;
+     **p2 = **p2+7;
+     cout<<n<<endl;
+     ***p3 = ***p3+11;
+     cout<<n<<endl;
+     return 0;
+}
