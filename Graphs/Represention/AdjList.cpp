e5f7f1a19ee9091c76883acab5f736eebfeb7e0a
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-05-16 11:42:10 +0530

    Update AdjList.cpp

--- Graphs/Represention/AdjList.cpp
+++ Graphs/Represention/AdjList.cpp
@@ -1,27 +1,27 @@
-// #include<iostream>
-// #include<vector>
-// using namespace std;
+#include<iostream>
+#include<vector>
+using namespace std;
 
-// int main() {
-//     int vertex, edges;
-//     cin >> vertex >> edges;
-//     vector<int> adjList[vertex];
-//     int u, v;
-//     for(int i = 0; i < edges; i++) {
-//         cin >> u >> v;
-//         adjList[u].push_back(v);
-//         adjList[v].push_back(u);
-//     }
-//     // printing
-//     for(int i = 0; i < vertex; i++) {
-//         cout << i << " ";
-//         for(int j = 0; j < adjList[i].size(); j++) {
-//             cout << adjList[i][j] << " ";
-//         }
-//         cout << endl;
-//     }
-//     return 0;
-// }
+int main() {
+    int vertex, edges;
+    cin >> vertex >> edges;
+    vector<int> adjList[vertex];
+    int u, v;
+    for(int i = 0; i < edges; i++) {
+        cin >> u >> v;
+        adjList[u].push_back(v);
+        adjList[v].push_back(u);
+    }
+    // printing
+    for(int i = 0; i < vertex; i++) {
+        cout << i << " ";
+        for(int j = 0; j < adjList[i].size(); j++) {
+            cout << adjList[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
 
 #include<iostream>
 #include<vector>
