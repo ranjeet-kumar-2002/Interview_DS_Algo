bfaeb55eccbad27e012a8382af261f6c40591780
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-05-16 08:49:59 +0530

    Update adjmatrix.cpp

--- Graphs/Represention/adjmatrix.cpp
+++ Graphs/Represention/adjmatrix.cpp
@@ -1,73 +1,75 @@
-// #include<iostream>
-// #include<vector>
-// using namespace std;
-// int main(){
-//      int vertex,edges;cin>>vertex>>edges;
-//      vector<vector<bool>>adjMat(vertex,vector<bool>(vertex,false));
-//      int v,u;
-//      for(int i = 0;i<edges;i++){
-//           cin>>u>>v;
-//           adjMat[u][v]=1;
-//           adjMat[v][u]=1;
-//      }
-//       for(int i =0;i<vertex;i++){
-//           for(int j = 0;j<vertex;j++){
-//               cout<<adjMat[i][j]<<" ";
-//           }
-//           cout<<endl;
-//      }
-//      return 0;
-// }
+// undirected_unweighted_graph
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+     int vertex,edges;cin>>vertex>>edges;
+     vector<vector<bool>>adjMat(vertex,vector<bool>(vertex,false));
+     int v,u;
+     for(int i = 0;i<edges;i++){
+          cin>>u>>v;
+          adjMat[u][v]=1;
+          adjMat[v][u]=1;
+     }
+      for(int i =0;i<vertex;i++){
+          for(int j = 0;j<vertex;j++){
+              cout<<adjMat[i][j]<<" ";
+          }
+          cout<<endl;
+     }
+     return 0;
+}
+
+// undirected_wseighted_graph
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+     int vertex,edges;cin>>vertex>>edges;
+     vector<vector<int>>adjMat(vertex,vector<int>(vertex,0));
+     int v,u,w;
+     for(int i = 0;i<edges;i++){
+          cin>>u>>v>>w;
+          adjMat[u][v]=w;
+          adjMat[v][u]=w;
+     }
+      for(int i =0;i<vertex;i++){
+          for(int j = 0;j<vertex;j++){
+              cout<<adjMat[i][j]<<" ";
+          }
+          cout<<endl;
+     }
+     return 0;
+}
 
 
 
-// #include<iostream>
-// #include<vector>
-// using namespace std;
-// int main(){
-//      int vertex,edges;cin>>vertex>>edges;
-//      vector<vector<int>>adjMat(vertex,vector<int>(vertex,0));
-//      int v,u,w;
-//      for(int i = 0;i<edges;i++){
-//           cin>>u>>v>>w;
-//           adjMat[u][v]=w;
-//           adjMat[v][u]=w;
-//      }
-//       for(int i =0;i<vertex;i++){
-//           for(int j = 0;j<vertex;j++){
-//               cout<<adjMat[i][j]<<" ";
-//           }
-//           cout<<endl;
-//      }
-//      return 0;
-// }
+// directed_unweighted_graph
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+     int vertex,edges;cin>>vertex>>edges;
+     vector<vector<int>>adjMat(vertex,vector<int>(vertex,0));
+     int v,u;
+     for(int i = 0;i<edges;i++){
+          cin>>u>>v;
+          adjMat[u][v]=1;
+     }
+      for(int i =0;i<vertex;i++){
+          for(int j = 0;j<vertex;j++){
+              cout<<adjMat[i][j]<<" ";
+          }
+          cout<<endl;
+     }
+     return 0;
+}
 
 
-
-
-
-// #include<iostream>
-// #include<vector>
-// using namespace std;
-// int main(){
-//      int vertex,edges;cin>>vertex>>edges;
-//      vector<vector<int>>adjMat(vertex,vector<int>(vertex,0));
-//      int v,u;
-//      for(int i = 0;i<edges;i++){
-//           cin>>u>>v;
-//           adjMat[u][v]=1;
-//      }
-//       for(int i =0;i<vertex;i++){
-//           for(int j = 0;j<vertex;j++){
-//               cout<<adjMat[i][j]<<" ";
-//           }
-//           cout<<endl;
-//      }
-//      return 0;
-// }
-
-
-
+// directed_weighted_graph
 #include<iostream>
 #include<vector>
 using namespace std;
