0c921071fdc4f5b1f86ecd162bc7c6311b407862
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-05-17 19:43:49 +0530

    Update bfs.cpp

--- Graphs/Traversal/BFS/bfs.cpp
+++ Graphs/Traversal/BFS/bfs.cpp
@@ -1,3 +1,56 @@
+
+#include<iostream>
+#include<queue>
+#include<vector>
+#include<cmath>
+#include<climits>
+using namespace std;
+class Solution {
+  public:
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        vector<int>visited(V,0);
+        queue<int>q;
+        q.push(0);
+        visited[0] = 1;
+        vector<int>ans;
+        while(!q.empty()){
+             int node = q.front();q.pop();
+             ans.push_back(node);
+             for(auto &nbr:adj[node]){
+                  if(!visited[nbr]){
+                      q.push(nbr);
+                      visited[nbr]=1;
+                  }
+             }
+        }
+          return ans;
+    }
+};
+int main(){
+     int vertex,edges;
+     cin>>vertex>>edges;
+     vector<int>adjList[vertex];
+     int u , v;
+     for(int i = 0;i<edges;i++){
+          cin>>u>>v;
+          adjList[u].push_back(v);
+          adjList[v].push_back(u);
+     }
+      Solution obj;
+      vector<int>ans = obj.bfsOfGraph(vertex,adjList);
+      for (int i = 0; i < ans.size(); i++) {
+        cout << ans[i] << " ";
+    }
+    cout << endl;
+     
+}
+    
+// V = 5, E = 4
+// adj = {{1,2,3},{},{4},{},{}}
+
+//***********************************************************************************//
+
+
 BFS of graph
 
 class Solution {
