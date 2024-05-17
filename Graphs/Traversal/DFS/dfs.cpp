22f175f6927a13b4dc95c4a3f8fd7de5ac8856f5
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-05-17 22:47:01 +0530

    Update dfs.cpp

--- Graphs/Traversal/DFS/dfs.cpp
+++ Graphs/Traversal/DFS/dfs.cpp
@@ -1,3 +1,50 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+#include<climits>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+
+class Solution {
+  public:
+    void dfs(int node,vector<int> adj[],vector<int>&ans,vector<int>&visited){
+         visited[node] = 1;
+         ans.push_back(node);
+         for(auto &nbr:adj[node]){
+              if(!visited[nbr]){
+                   dfs(nbr,adj,ans,visited);
+              }
+         }
+    }
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+        vector<int>visited(V,0);
+        vector<int>ans;
+        dfs(0,adj,ans,visited);
+        return ans;
+    }
+};
+
+int main(){
+     int vertex,edges;
+     cin>>vertex>>edges;
+     vector<int>adjlist[vertex];
+     int u ,v;
+     for(int i = 0;i<edges;i++){
+          cin>>u>>v;
+          adjlist[u].push_back(v);
+          adjlist[v].push_back(u);
+     }
+     Solution obj;
+     vector<int>ans = obj.dfsOfGraph(vertex,adjlist);
+     for(int i = 0;i<ans.size();i++){
+          cout<<ans[i]<<" ";
+     }
+     return 0;
+}
+
+**************************************************************************************
+
 class Solution {
   public:
     void dfs(int src,vector<bool>&visited,vector<int>adj[],vector<int>&ans){
@@ -13,6 +60,25 @@ class Solution {
         vector<int>ans;
         vector<bool>visited(V,false);
             dfs(0,visited,adj,ans);
+        return ans;
+    }
+};
+//****************************************************************************//
+class Solution {
+  public:
+    void dfs(int node,vector<int> adj[],vector<int>&ans,vector<int>&visited){
+         visited[node] = 1;
+         ans.push_back(node);
+         for(int j = 0;j<adj[node].size();j++){
+              if(!visited[adj[node][j]]){
+                  dfs(adj[node][j],adj,ans,visited);
+              }
+         }
+    }
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+        vector<int>visited(V,0);
+        vector<int>ans;
+        dfs(0,adj,ans,visited);
         return ans;
     }
 };
