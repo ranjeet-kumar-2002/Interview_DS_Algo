e5238f4821d200b4ac4e1c446cdf0e18b7cebe64
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-05-31 09:15:23 +0530

    Create Bipartite.cpp

---
+++ Graphs/Bipartite.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+using namespace std;
+    bool bipartite(int src,vector<int>&color,vector<int>adj[]){
+         queue<int>q;
+         q.push(src);
+         color[src] = 0; // Blue color 
+         while(!q.empty()){
+              int node = q.front();q.pop();
+              for(auto &nbr:adj[node]){
+                   if(color[nbr]==-1){
+                        color[nbr] = (color[node]+1)%2;
+                        q.push(nbr);
+                   }else {
+                        
+                        if(color[nbr] == color[node]) return 0;
+                        
+                   }
+              }
+         }
+         return true;
+    }
+	bool isBipartite(int V, vector<int>adj[]){
+	    vector<int>color(V,-1);
+	    for(int i = 0;i<V;i++){
+	            if(color[i]==-1){
+	                  if(!bipartite(i,color,adj)) return 0;
+	            }
+	    }
+	    return true;
+	}
+int main(){
+     int v,e;
+     cin>>v>>e;
+     vector<int>adj[v];
+     for(int i =0;i<e;i++){
+          int u,v;
+          cin>>u>>v;
+          adj[u].push_back(v);
+          adj[v].push_back(u);
+     }
+     cout<<isBipartite(v,adj);
+     return 0;
+}
