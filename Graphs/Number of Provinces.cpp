3ab40a356bcfa9d5101b16c23d2e4ee0b9b67fa3
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-05-31 17:23:06 +0530

    Update Number of Provinces.cpp

--- Graphs/Number of Provinces.cpp
+++ Graphs/Number of Provinces.cpp
@@ -1,3 +1,54 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+using namespace std;
+    void dfs(int src,vector<int>&visited, vector<int>adjlist[]){
+         visited[src]= 1;
+         for(auto &nbr:adjlist[src]){
+              if(!visited[nbr]){
+                  dfs(nbr,visited,adjlist);
+              }
+         }
+    }
+    int numProvinces(vector<vector<int>> adj, int V) {
+      vector<int>adjlist[V];
+      for(int i = 0;i<adj.size();i++){
+          for(int j = 0;j<adj[0].size();j++){
+              if(adj[i][j]==1){
+                  adjlist[i].push_back(j);
+                  adjlist[j].push_back(i);
+              }
+          }
+      }
+      int cc = 0;
+      vector<int>visited(V,0);
+      for(int i =0;i<V;i++){
+          if(!visited[i]) {
+              dfs(i,visited,adjlist);
+              cc++;
+          }
+      }
+      return cc;
+    }
+
+int main(){
+    int m,n;cin>>m>>n;
+    vector<vector<int>>adj(m,vector<int>(n));
+    for(int i =0;i<m;i++){
+        for(int j =0;j<n;j++){
+            cin>>adj[i][j];
+        }
+    }
+    cout<<"enter no of vertex"<<endl;
+    int vertex;cin>>vertex;
+    cout<<numProvinces(adj,vertex);
+    return 0;
+}
+
+****************************************************************************************
+
+
+
 class Solution {
   public:
     void dfs(int src,vector<int>&visited, vector<int>adjlist[]){
@@ -30,7 +81,7 @@ class Solution {
     }
 };
 
-
+*******************************************************************************************************
 class Solution {
   public:
     void bfs(int src,vector<int>&visited,  vector<int>adjlist[]){
