034eb0d62be4d8fd5eb0e3335fd41a292207a57c
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-05-31 15:43:12 +0530

    Create X Total Shapes.cpp

---
+++ Graphs/Multi sourceBFS/X Total Shapes.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<string>
+using namespace std;
+    vector<vector<int>>dir = {{0,1},{1,0},{-1,0},{0,-1}};
+    void bfs(int row ,int col,vector<vector<char>>& grid){
+        queue<pair<int,int>>q;
+        q.push({row,col});
+        grid[row][col] = 'O';
+        while(!q.empty()){
+            int size = q.size();
+            for(int k = 0;k<size;k++){
+                 auto x = q.front();q.pop();
+                 int i = x.first;
+                 int j = x.second;
+                 for(int d = 0;d<4;d++){
+                     int newi = i+dir[d][0];
+                     int newj = j+dir[d][1];
+                     if(newi>=0 && newj>=0 && newi<grid.size() && newj<grid[0].size() && grid[newi][newj]=='X'){
+                        grid[newi][newj] = 'O';
+                        q.push({newi,newj});
+                     }
+                 }
+            }
+            
+        }
+    }
+    int xShape(vector<vector<char>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int ans = 0;
+        for(int i = 0;i<m;i++){
+            for(int j =0;j<n;j++){
+                if(grid[i][j]=='X'){
+                  bfs(i,j,grid);
+                  ans++;
+                }
+            }
+        }
+        return ans;
+    }
+int main(){
+    int m,n;cin>>m>>n;
+    vector<vector<char>>grid(m,vector<char>(n));
+    for(int i = 0;i<m;i++){
+        for(int j =0;j<n;j++){
+            cin>>grid[i][j];
+        }
+    }
+    cout<<xShape(grid);
+    return 0;
+}
