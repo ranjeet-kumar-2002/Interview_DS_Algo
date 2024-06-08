6c2ba6c1b1b4438681ce3859294e28d0a4711a6c
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-06-08 13:52:08 +0530

    Create constructor.cpp

---
+++ OOP/constructor.cpp
@@ -0,0 +1,161 @@
+#include<iostream>
+#include<cmath>
+#include<climits>
+#include<algorithm>
+#include<vector>
+using namespace std;
+class customer{
+    string name;
+    int acc_no;
+    int balance;
+    public:
+    customer(){  // default constructor
+        cout<<"customer constructor called"<<endl;
+    }
+};
+
+int main(){
+    customer A1;
+    return 0;
+}
+
+***********************************************************//
+#include<iostream>
+#include<cmath>
+#include<climits>
+#include<algorithm>
+#include<vector>
+using namespace std;
+class customer{
+    string name;
+    int acc_no;
+    int balance;
+    public:
+    customer(){  // default constructor
+      name = "Ranjeet";
+      acc_no = 100;
+      balance = 10000000;
+    }
+    void display(){
+        cout<<name<<" "<<acc_no<<" "<<balance<<endl;
+    }
+};
+
+int main(){
+    customer A1,A2;
+    A1.display();
+    A2.display();
+    return 0;
+}
+
+//************************************************************//
+// if function is same but parameters are different called constructor overloading 
+
+#include<iostream>
+using namespace std;
+class customer{
+    string name;
+    int acc_no;
+    int balance;
+    public:
+    customer(string a,int b,int c){ //parameterized constructor
+      name = a;
+      acc_no = b;
+      balance = c;
+    }
+    
+     customer(string a,int b){ //parameterized constructor
+      name = a;
+      acc_no = b;
+    }
+    
+    void display(){
+        cout<<name<<" "<<acc_no<<" "<<balance<<endl;
+    }
+};
+int main(){
+    customer A1("Ranjeet",11,11111);
+    A1.display();
+}
+
+
+//************************************************// 
+
+#include<iostream>
+using namespace std;
+class customer{
+    string name;
+    int acc_no;
+    int balance;
+    public:
+    customer(string name,int acc_no,int balance){ //parameterized constructor
+      this->name = name;
+      this->acc_no = acc_no;
+      this->balance = balance;
+    }
+    void display(){
+        cout<<name<<" "<<acc_no<<" "<<balance<<endl;
+    }
+};
+int main(){
+    customer A1("Ranjeet",11,11111);
+    A1.display();
+}
+
+
+//********************inline constructor ************************//
+
+#include<iostream>
+using namespace std;
+class customer {
+    string name;
+    int acc_no;
+    int balance;
+public:
+    // Inline parameterized constructor
+  inline customer(string name, int acc_no, int balance) : name(name), acc_no(acc_no), balance(balance) {
+    }
+    void display() {
+        cout << name << " " << acc_no << " " << balance << endl;
+    }
+};
+
+int main() {
+    customer A1("Ranjeet", 11, 11111);
+    A1.display();
+    return 0;
+}
+
+//*********************copy*****************************//
+// A constructor that is use to copy or inialize the value of one object into another object is called copy constructor
+
+
+#include<iostream>
+using namespace std;
+class customer {
+int id;
+public:
+customer(int a){
+     id = a;
+}
+customer(customer &x){
+      id = x.id;
+}
+ 
+ void display(){
+     cout<<"the value of x is "<<id<<endl;
+ }
+ 
+};
+
+int main() {
+   customer a(100);
+   customer b(a); // copy constructor call method 1
+   customer c = a; // copy constructor call method 1
+   cout<<"display first object"<<endl;
+   a.display();
+   cout<<"display second object"<<endl;
+   b.display();
+   cout<<"display third object"<<endl;
+   c.display();
+}
